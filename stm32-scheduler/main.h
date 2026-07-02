eb57ababe5df75af0ba46ac49d5f00283599a62f
Aniketh <anikethofficial1@gmail.com>
2026-07-02 21:45:37 +0530

    Update main.h
    updated with blocking,ready and idle states

--- stm32-scheduler/main.h
+++ stm32-scheduler/main.h
@@ -2,28 +2,33 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#define MAX_TASKS			4U
-                               /*stack memory calculation*/
+#define MAX_TASKS		4U //3 user tasks and 1 idle task
 
-#define SIZE_TASK_STACK 	1024U   //1KB
-#define SIZE_SCHED_STACK    1024U
+/*some stack memory calculations*/
+#define SIZE_TASK_STACK     1024U //1kb
+#define SIZE_SCHED_STACK    1024U //1kb
 
-#define SRAM_START			0x20000000U //from memory map (rm0390)
-#define SIZE_SRAM			((128) * (1024))  //128KB FOR THE BOARD
-#define SRAM_END			((SRAM_START) + (SIZE_SRAM))   //RAM END, SINCE WE DECIDED T1 IS AT RAM END
+#define SRAM_START          0x20000000U
+#define SIZE_SRAM           ((128) * (1024)) //128kb
+#define SRAM_END            ((SRAM_START) + (SIZE_SRAM)) //end of ram = start of t1 stack
 
-#define T1_STACK_START		SRAM_END
-#define T2_STACK_START		((SRAM_END) - (SIZE_TASK_STACK))//'-' BECAUSE STACK CONSUMPTION IS FROM BACK TO FRONT
-#define T3_STACK_START		((SRAM_END) - (2 * SIZE_TASK_STACK))
-#define T4_STACK_START		((SRAM_END) - (3 * SIZE_TASK_STACK))
-#define SCHED_START_STACK   ((SRAM_END) - (4 * SIZE_TASK_STACK))
-						////////////////////////////////////////////////////
+#define T1_STACK_START      SRAM_END
+#define T2_STACK_START      ((SRAM_END) - (SIZE_TASK_STACK)) //since stack full descending
+#define T3_STACK_START      ((SRAM_END) - (2 * SIZE_TASK_STACK))
+#define IDLE_STACK_START	((SRAM_END) - (3 * SIZE_TASK_STACK))
+#define SCHED_STACK_START      ((SRAM_END) - (4 * SIZE_TASK_STACK))
 
-#define TICK_HZ				1000U
-#define HSI_CLK				16000000U  //->16MHz
-#define SYSTICK_TIM_CLK		HSI_CLK
+#define TICK_HZ				1000U  //desireed exception frequency
 
-#define DUMMY_XPSR	0x01000000U  //to run in thumb state
+#define HSI_CLOCK			16000000U //16MHZ
+#define SYSTICK_TIM_CLK		HSI_CLOCK
 
+#define DUMMY_XPSR		0x01000000U
+
+#define TASK_READY_STATE 0x00
+#define TASK_BLOCKED_STATE 0xFF
+
+#define INTERRUPT_DISABLE()		do{__asm volatile ("MOV R0,#0x1"); __asm volatile("MSR PRIMASK,R0");}while(0)
+#define INTERRUPT_ENABLE()		do{__asm volatile ("MOV R0,#0x0"); __asm volatile("MSR PRIMASK,R0");}while(0)
 
 #endif /* MAIN_H_ */
